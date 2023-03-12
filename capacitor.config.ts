e2569c1ce4e8caee07394d4a719d6ebdff10c751
unknown <bartekwikiera@gmail.com>
2023-03-12 16:18:26 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'E8 Matematyka',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
